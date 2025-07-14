48edd5912bbe880718a6b66af78ed6d2fbd77331
pntt3011 <thanhtung30112001@gmail.com>
2025-07-14 17:40:42 +0700

    Add homepage v1.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,30 @@
 <section>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a suscipit
-  ipsum. Pellentesque dapibus ullamcorper dapibus. Nam eleifend egestas rutrum.
-  Aliquam tempus aliquet ultricies. Morbi pulvinar vestibulum varius. Praesent
-  at hendrerit nisi. Cras tincidunt lectus sed magna semper, vel porta nunc
-  blandit. Ut accumsan fringilla libero non sagittis. Proin at pulvinar dolor,
-  fermentum ultricies mi. Nunc eu pellentesque nibh. Suspendisse ut vestibulum
-  quam, et vehicula neque. Duis aliquam fringilla tortor quis lacinia. Donec
-  accumsan ipsum efficitur finibus consectetur. Vivamus accumsan nibh at dui
-  tristique, ut molestie enim efficitur. Vestibulum fringilla a nisi ut egestas.
-  Nullam et turpis sed libero vestibulum tempor eu vel tortor. Vivamus vehicula
-  mattis varius. Etiam at hendrerit felis, molestie maximus tortor. Vivamus
-  maximus purus quis laoreet ullamcorper. Morbi sagittis justo eget accumsan
-  vulputate. Proin massa sapien, blandit id fringilla molestie, iaculis ac ex.
-  In molestie pulvinar lacus, vel facilisis est iaculis sed. Integer at ante eu
-  elit sodales molestie. Integer consectetur, nisl et auctor viverra, felis erat
-  sodales libero, non luctus massa felis et lacus. Curabitur lectus sem,
-  elementum quis feugiat eget, aliquet molestie enim. Nullam ac luctus risus,
-  sit amet sodales nisi. Nam pulvinar fringilla tristique. Integer magna turpis,
-  tempus vitae feugiat et, pharetra a diam. Donec vitae ipsum tellus.
-  Pellentesque eget vehicula tortor. Etiam faucibus felis porta turpis feugiat
-  facilisis. Nulla commodo, risus eget tempus lacinia, magna sem cursus turpis,
-  sit amet egestas tellus purus vel erat. Morbi placerat ornare risus, sit amet
-  hendrerit erat faucibus sit amet. Integer id lorem quis dolor faucibus
-  imperdiet. Donec nec purus at tellus faucibus condimentum. Praesent fringilla
-  dui nisl. Phasellus diam lorem, tincidunt quis eros non, consequat imperdiet
-  turpis. Nam feugiat felis lobortis enim condimentum, ac consectetur ipsum
-  commodo. Aliquam pharetra hendrerit mauris sed vulputate. Maecenas egestas
-  laoreet leo, et convallis dolor pulvinar quis. Nam hendrerit a ipsum et
-  mattis. Pellentesque sit amet eros nibh. Mauris ac feugiat lorem. Mauris arcu
-  massa, lobortis vitae est quis, sodales interdum nunc. Mauris eu vestibulum
-  magna. Etiam quis gravida lacus. Nullam in sodales diam. Nulla facilisi.
-  Suspendisse aliquam, tortor vel mattis euismod, orci ligula semper velit,
-  molestie facilisis est justo sed est. Maecenas fringilla metus eget faucibus
-  mattis. Integer finibus tellus egestas, egestas elit aliquam, laoreet lorem.
-  Aliquam euismod convallis est, in vehicula risus ultrices in. Ut nisl odio,
-  pharetra sit amet fringilla ac, faucibus at nisl. Nullam dictum efficitur
-  pulvinar. Nullam pulvinar eros quis libero vestibulum rutrum. Nullam vitae
-  tempor mi. Sed dolor metus, eleifend nec nunc eu, placerat lacinia est. Proin
-  dignissim, augue a dapibus rhoncus, mauris tellus tempor nisl, fermentum
-  ullamcorper metus nunc in erat. Lorem ipsum dolor sit amet, consectetur
-  adipiscing elit. Integer tempor convallis odio, in dignissim ex rhoncus vel.
+  <p>
+    Hello I'm Tung Phan. I live in Vietnam and recently graduated from
+    university.<br />
+    To be honest, I feel a bit lost right now.<br />
+    There isn't any particular thing I want to do.<br />
+    I don't know what kind of person I want to become in the future.<br />
+    That's why I can't tell you more about myself at the moment.
+  </p>
+  <p>
+    However, I do know that I love writing, learning and helping others. That's
+    why I decide to start this blog.<br />
+    Each post will be about a project or idea related to a certain topic. These topics
+    are not determined, as I'll write about anything that captures my interest at
+    the time.<br />
+    I hope you gain something new by reading my posts. It'd be even greater if you
+    can apply these things into your daily life or work. That's my ultimate goal
+    of this blog.
+  </p>
+  <p>
+    I expect you can get most of my blog without any prior knowledge. Some tools
+    or topics may require certain knowledge to fully understand. However,
+    technologies can change over time, what's really matter is the way we
+    approach and solve problems. That's the core messege I want to emphasize
+    throughout this blog.
+  </p>
+  <p>Let's enjoy this journey together.</p>
 </section>
 
 <style>
@@ -47,5 +32,9 @@
     text-align: justify;
     text-justify: inter-word;
     line-height: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    color: #323334;
   }
 </style>
