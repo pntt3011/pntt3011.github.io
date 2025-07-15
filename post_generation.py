be5ad65998094feeac121b0e8257b5428ae779cf
pntt3011 <thanhtung30112001@gmail.com>
2025-07-15 16:49:08 +0700

    Add metadata generator

--- post_generation.py
+++ post_generation.py
@@ -4,6 +4,48 @@ import re
 from markdown_it import MarkdownIt
 from mdit_py_plugins.front_matter import front_matter_plugin
 from PIL import Image
+from datetime import datetime
+
+class Post:
+	def __init__(self, filename: str, directory: str):
+		self.filename = filename
+		self.dir = directory
+		self.path = os.path.join(directory, filename)
+		self.id = generate_markdown_id(self.path)
+		self.creation_time = 0
+		self.title = ""
+		self.tags: list[str] = []
+		self.preview = ""
+		self._try_parse_metadata()
+
+	def is_valid(self) -> bool:
+		return self.title != "" and self.creation_time > 0 and self.id != "-1" and len(self.tags) > 0
+
+	def _try_parse_metadata(self):
+		if os.path.exists(self.path):
+			try:
+				self.creation_time = int(os.path.getctime(self.path))
+
+				with open(self.path, 'r', encoding='utf-8') as file:
+					md_content = file.read()
+					lines = md_content.splitlines()
+					if lines and lines[0].startswith('---'):
+						end_index = lines.index('---', 1)
+						if end_index > 0:
+							for line in lines[1:end_index]:
+								if ':' in line:
+									key, value = line.split(':', 1)
+									key = key.strip().lower()
+									value = value.strip()
+
+									if key == "title":
+										self.title = value
+									elif key == "tags":
+										self.tags = [tag.strip() for tag in value.split(',')]
+									elif key == "preview":
+										self.preview = value.strip()
+			except Exception as e:
+				print(f"Error reading metadata from {self.path}: {e}")
 
 class Photo:
 	def __init__(self, filename: str, directory: str):
@@ -53,13 +95,15 @@ class Photo:
 		return target_photo
 
 class Config:
-	def __init__(self, markdown_dir: str, md_media_dir: str, html_dir: str, html_media_dir: str, image_size_max: int, image_size_percentage: float):
+	def __init__(self, markdown_dir: str, md_media_dir: str, html_dir: str, html_media_dir: str, image_size_max: int, image_size_percentage: float, data_dir: str):
 		self.markdown_dir = markdown_dir
 		self.md_media_dir = md_media_dir
 		self.html_dir = html_dir
 		self.html_media_dir = html_media_dir
 		self.image_size_max = image_size_max
 		self.image_size_percentage = image_size_percentage
+		self.post_metadata_path = os.path.join(data_dir, "posts.json")
+		self.tag_metadata_path = os.path.join(data_dir, "tags.json")
 
 def generate_html_for_markdown_folder(config: Config) -> None:
 	for file in os.listdir(config.markdown_dir):
@@ -195,15 +239,71 @@ def decorate_generated_html_with_css(html_content: str) -> str:
 	return svelte_content + css_content
 
 def get_svelte_directory(md_path: str, html_dir: str) -> str:
-	file_name = os.path.basename(md_path).replace('.md', '')
-	svelte_dir = os.path.join(html_dir, file_name)
+	folder_name = generate_markdown_id(md_path)
+	svelte_dir = os.path.join(html_dir, folder_name)
 	return svelte_dir
+
+def generate_markdown_id(md_path: str) -> str:
+	try:
+		stat = os.stat(md_path)
+		return datetime.fromtimestamp(stat.st_birthtime).strftime("%Y%m%d%H%M%S")
+	except Exception as e:
+		print(f"Error generating ID for {md_path}: {e}")
+		return "-1"
 
 def write_svelte_content_to_file(svelte_content: str, svelte_dir: str) -> None:
 	os.makedirs(svelte_dir, exist_ok=True)
 	svelte_path = os.path.join(svelte_dir, "+page.svelte")
 	with open(svelte_path, "w", encoding="utf-8") as svelte_file:
 		svelte_file.write(svelte_content)
+
+def generate_metadata_for_markdown_folder(config: Config) -> None:
+	post_metadata = get_post_metadata(config.markdown_dir)
+	write_post_metadata(post_metadata, config.post_metadata_path)
+	tag_metadata = parse_tag_metadata(post_metadata)
+	write_tag_metadata(tag_metadata, config)
+
+def get_post_metadata(md_dir: str) -> list[Post]:
+	metadata = []
+	for filename in os.listdir(md_dir):
+		if filename.endswith('.md'):
+			post = Post(filename, md_dir)
+			if post.is_valid():
+				metadata.append(post)
+	metadata.sort(key=lambda x: x.creation_time, reverse=True)
+	return metadata
+
+def write_post_metadata(post_metadata: list[Post], post_metadata_path: str) -> None:
+	json_data = [{
+		"id": post.id,
+		"title": post.title,
+		"creation_time": post.creation_time,
+		"tags": post.tags,
+		"preview": post.preview,
+	} for post in post_metadata if post.is_valid()]
+
+	os.makedirs(os.path.dirname(post_metadata_path), exist_ok=True)
+	with open(post_metadata_path, 'w', encoding='utf-8') as file:
+		import json
+		json.dump(json_data, file, indent=4, ensure_ascii=False)
+
+def parse_tag_metadata(post_metadata: list[Post]) -> list[dict[str, int]]:
+	tag_dict = {}
+	for post in post_metadata:
+		for tag in post.tags:
+			if tag not in tag_dict:
+				tag_dict[tag] = 0
+			tag_dict[tag] += 1
+	tag_metadata = [{"tag": tag, "count": count} for tag, count in tag_dict.items()]
+	tag_metadata.sort(key=lambda x: (-x["count"], x["tag"].lower()))
+	tag_metadata.insert(0, {"tag": "all", "count": len(post_metadata)})
+	return tag_metadata
+
+def write_tag_metadata(tag_metadata: list[dict[str, int]], config: Config) -> None:
+	os.makedirs(os.path.dirname(config.tag_metadata_path), exist_ok=True)
+	with open(config.tag_metadata_path, 'w', encoding='utf-8') as file:
+		import json
+		json.dump(tag_metadata, file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
 	config = Config(
@@ -212,6 +312,8 @@ if __name__ == "__main__":
 		html_dir="src/routes/posts",
 		html_media_dir="static/media",
 		image_size_max=1024,
-		image_size_percentage=0.8
+		image_size_percentage=0.8,
+		data_dir="src/lib"
 	) 
 	generate_html_for_markdown_folder(config)
+	generate_metadata_for_markdown_folder(config)
