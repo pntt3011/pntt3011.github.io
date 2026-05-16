50cb101e4d444af50bd8fbb2fdea6965008d1083
pntt3011 <thanhtung30112001@gmail.com>
2026-05-16 14:40:12 +0700

    Update algorithm

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -39,7 +39,7 @@ struct State {
     stock_qty: u32,
 }
 
-const MAX_PRIMARY_PATTERNS: usize = 7;
+const MAX_PRIMARY_PATTERNS: usize = 5;
 const BEAM_WIDTH: usize = 200;
 const CANDIDATES_PER_STATE: usize = 80;
 const RANDOM_CANDIDATE_COUNT: usize = 80;
